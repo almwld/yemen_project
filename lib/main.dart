b796fbf94884873a47e09bbfd3b5159758aec21e
YemenMarketDev <yemenmarket@example.com>
2026-01-23 10:38:13 +0300

    Fix root widget name (YemenMarketApp)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-
-import 'services/guest_auth_service.dart';
 import 'app.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await GuestAuthService.signIn();
+void main() {
   runApp(const YemenMarketApp());
 }
