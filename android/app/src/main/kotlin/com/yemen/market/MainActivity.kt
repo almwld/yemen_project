9db364d927cae6ec743cdf1ba764e569d67c8686
almwld <your-email@example.com>
2026-02-06 02:29:59 +0300

    Fix: Rebuild android folder to support new Gradle version

--- android/app/src/main/kotlin/com/yemen/market/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.yemen.market
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity() {}
--- android/app/src/main/kotlin/com/yemenmarket/MainActivity.kt
+++
@@ -1,3 +0,0 @@
-package com.yemenmarket
-import io.flutter.embedding.android.FlutterActivity
-class MainActivity: FlutterActivity() {}
